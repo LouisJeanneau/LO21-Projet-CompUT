4183207841980616ac856e3268284abbf7df0f56
Louis Jeanneau <louis.jeanneau@etu.utc.fr>
2020-12-07 12:25:55 +0100

    mise au propre du Gitlab

---
+++ code/main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Louis on 06/12/2020.
+//
+
+#include <iostream>
+#include <QApplication>
+#include <QPushButton>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    QPushButton button("Hello world!", nullptr);
+    button.resize(200, 100);
+    button.show();
+    return QApplication::exec();
+}
