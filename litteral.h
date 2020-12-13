2092b16a718bcb751ec86e987fc679cad61d08d0
Huiling Li <huiling.li@etu.utc.fr>
2020-12-13 20:00:19 +0100

    Replace litteral.h

--- litteral.h
+++ litteral.h
@@ -3,16 +3,15 @@
 
 #endif // LITTERAL_H
 #include <QString>
+#include<iostream>
 using namespace std;
-//识别 创造
-//动态分配 释放
 //package "GroupeLitterale" {
 
 
-//  class ConstructeurLitterale: public Litterale {
+//  class ConstructeurLitterale: public Litterale {//string,distinguer le type
 //    char[30] chaine;
 //    public:
-//      ConstructeurLitterale();
+//    ConstructeurLitterale();
 //    constructeur_entier;
 //    constructeur_reel;
 //    constructeur_atome;
@@ -20,13 +19,16 @@ using namespace std;
 //  }
 
 
-//ajouterAtome()
 
 
 class Litterale {
 public:
     virtual ~Litterale() = default;
     virtual QString versString() const = 0;
+//    void afficher(std::ostream& f=std::cout)const
+//    {
+//        f << versString();
+//    }
 };
 
 class Numerique:public Litterale{
@@ -45,62 +47,46 @@ public:
     ~Reel();
 };
 
-class Rationel: public Numerique{
+class Rationnel: public Numerique{
     int if_simple=0;
     int numerateur;
-    int denominateur; //不一样的要确定的才要记录
+    int denominateur;
     //char slash;
 public:
-
-    //    void simplifier(int*n,int *d) {
-    //      //if (denominateur == 1) // ou après  simplification ça devient un entier ：interpreteur doit le vérifier d＇ abord
-    //      if(*n==1)return;//pas besion de simplifier
-    //      for(int i=2;i<=*n&&i<=*d;i++)
-    //      {
-    //          if((*n)%i==0&&(*d)%i==0)
-    //          {
-    //            *n=(*n)/i;
-    //            *d=(*d)/i;
-    //            simplifier(&n,&d);
-    //          }
-    //      }
+    void simplifier(int*n,int *d) ;
     int getNumerateur()const{return numerateur;}
     int getDenominateur()const{return denominateur;}
     int ifSimple()const{return if_simple;}
-    Rationel(int n,int d)
+    Rationnel(int* n,int* d)
     {
-        simplifier(&n,&d);
-        numerateur=n;
-        denominateur=d;
+        simplifier(n,d);
+        numerateur=*n;
+        denominateur=*d;
     }
-    ~Rationel();
+    QString versString() const;
+    ~Rationnel();
 };
 
 class Entier : public Numerique{
-    //char signe;
     int entier;
 public:
-    Entier(int i): entier(i) {}; //默认的是干嘛的
+    Entier(int i): entier(i) {};
     int getEntier()const {return entier;}
     void negative() //operateur NEG
     {
         entier = -entier;
     }
     QString versString() const;
-    ~Entier(); //什么时候不适用了
+    ~Entier();
 };
 
 class Programme : public Litterale{
     //char crochet1,crochet2;
     QString programme;//y compris les sous-programme,il faut interpréter
-    int longeur;
     int delimitateur;
     //Programme * sousProgramme = nullptr;
 public:
-    Programme(QString p)
-    {
-        strcpy(programme,p);
-    }
+    Programme(QString s) : programme(std::move(s)) {}
     QString getProgramme()const {return programme;};
     ~Programme();
     QString versString() const;
@@ -115,35 +101,40 @@ public:
 //    }
 };
 
+
 class Atome : public Litterale{
     QString atome;//[ ] pas compris
-    int longeur;
     int delimitateur;
     int fonction=0; //indiquer le role joue:0:non associé（par defaut） 1 :identificateur de numerique 2:identificateur de programme
     Programme * p = nullptr;
     Numerique * n = nullptr; //au cas de bouton
-    Atome * suivant=nullptr; //pour chercher:
+    //Atome * suivant=nullptr; //pour chercher:
     //Expression*??
 public:
     QString getAtome()const {return atome;}
     int getfonction ()const{return fonction;}
-    void Associer(Programme* pro = nullptr,Numerique* nu = nullptr){p=pro;n=nu;}//initialiser ou remplacer l'association
-    Atome(QString a,int l)
-    {
-        strcpy(atome,a);
-        longeur=l;
+    void Associer(Programme* pro = nullptr,Numerique* nu = nullptr){
+        if(pro!=nullptr)
+        {
+            fonction=2;
+            p=pro;}
+        if(nu!=nullptr)
+        {
+            fonction=1;
+            n=nu;
+        }//initialiser ou remplacer l'association'
     }
+    explicit Atome(QString s) : atome(std::move(s)) {}
     int ifDejaIdentifi(){return fonction==1||fonction==2 ;}; //sinon créer une expression???
-    //    Remplace();
-    int ifCorrectSyntaxe(QString a)
-    {
-        if(a[0]<'A'||a[0]>'Z')return 0;
-        for(int i=1;a[i]!='\0';i++)
-            if(a[i]<'A'||a[i]>'Z'||(a[i]<='9'&&a[i]>='0'))
-                return 0;//pas Majuscule ,il faut rectifier
+//    int ifCorrectSyntaxe(QString a)
+//    {
+//        if(a[0]<'A'||a[0]>'Z')return 0;
+//        for(int i=1;a[i]!='\0';i++)
+//            if(a[i]<'A'||a[i]>'Z'||(a[i]<='9'&&a[i]>='0'))
+//                return 0;//pas Majuscule ,il faut rectifier
 
-        return 1;
-    }
+//        return 1;
+//    }
 
     int ifPredefini(QString a)//1:predefini,il faut rectifier
     {
@@ -161,8 +152,9 @@ public:
                 QString::compare(a,"STO")||\
                 QString::compare(a,"FORGET"))
             return 1;
+        else return 0;
     }
-    //toString()
+
 //    void afficherAtome()
 //    {
 //        for(int i=0;atome[i]!='\0'&&i<=delimitateur;i++)
@@ -175,9 +167,18 @@ public:
     void effacer(){p=nullptr;n=nullptr;}    //peuvent effacé par FORGET
 };
 
+//class ListeAtome{
+//    Atome* debut;
+//    ListeAtome(const ListeAtome &la) = delete;
+//    ListeAtome& operator=(const ListeAtome &la) = delete;
+//    static ListeAtome instance;
+
+//public:
+
+//    ListeAtome* ajouterAtome();
+//};
 
 class Expression : public Litterale{
-    //char guillmet1,guillmet2;
     Atome * atome; //identifier
 public:
     Expression(Atome* a): atome(a) {};
@@ -192,4 +193,3 @@ public:
 //    }
     QString versString() const;
 };
-
