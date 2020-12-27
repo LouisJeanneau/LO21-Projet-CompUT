4632d7d587153f6bb466a6a67c999204c5933e35
Lorenzo Zitoli <lorenzo.zitoli@etu.utc.fr>
2020-12-27 12:18:18 +0100

    Ajout gestion programmes Persistence

--- code/Persistence.h
+++ code/Persistence.h
@@ -1,17 +1,22 @@
 #ifndef PERSISTENCE_H
 #define PERSISTENCE_H
+
 #include <QMap>
 #include <QWidget>
 
 class Persistence
 {
+
 public:
     static QMap<QString, QString> mapVariable;
     static QMap<QString, QString> mapProgramme;
     Persistence();
-    void setMapVariable(QString atome,QString variable){mapVariable.insert(atome,variable);};
-    QMap<QString, QString> getMapVariable(){return mapVariable;};
-    unsigned int getMapVariableSize(){return mapVariable.size();};
+    void setMapVariable(QString atome,QString variable){mapVariable.insert(atome,variable);}
+    QMap<QString, QString> getMapVariable(){return mapVariable;}
+    unsigned int getMapVariableSize(){return mapVariable.size();}
+    void setMapProgramme(QString atome,QString programme){mapProgramme.insert(atome,programme);}
+    QMap<QString, QString> getMapProgramme(){return mapProgramme;}
+    unsigned int getMapProgrammeSize(){return mapProgramme.size();}
 };
 
 #endif // PERSISTENCE_H
