44b47cd67e5666bfcafe5db011d7a40e37fe68d9
kadiriis <ismail.kadiri@etu.utc.fr>
2021-01-04 13:10:55 +0100

    Légère modif Operateur

---
+++ code/Persistance.cpp
@@ -0,0 +1,68 @@
+#include "Persistance.h"
+#include "Exception.h"
+
+using namespace std;
+
+Persistance &Persistance::obtenirPersistance() {
+    static Persistance instance;
+    return instance;
+}
+
+void Persistance::ajouterVariable(QString saisieAtome, QString saisieVariable) {
+    if (saisieAtome != NULL && saisieVariable != NULL) {
+        if (saisieAtome == '0' || saisieAtome == '1' || saisieAtome == '2' || saisieAtome == '3' ||
+            saisieAtome == '4' ||
+            saisieAtome == '5' || saisieAtome == '6' || saisieAtome == '7' || saisieAtome == '8' ||
+            saisieAtome == '9' ||
+            saisieAtome == '+' || saisieAtome == '-' || saisieAtome == '*' || saisieAtome == '/' ||
+            saisieAtome == "CLEAR" || saisieAtome == "EVAL") {
+            throw ComputerException("Cet atome ne peut pas être réutilisé.");
+        } else {
+            if (saisieVariable.contains(' ')) {
+                throw ComputerException("Rentrez une unique expression sans espace pour une variable.");
+            }
+            if (mapProgramme.contains(saisieAtome))
+                throw ComputerException("Ce nom est déjà lié à un programme");
+            else {
+                mapVariable.insert(saisieAtome, saisieVariable);
+                emit actualiserAffichage();
+                return;
+            }
+        }
+    } else {
+        throw ComputerException("Remplissez tous les champs avant de valider.");
+    }
+}
+
+void Persistance::supprimerVariable(QString atome) {
+    mapVariable.remove(atome);
+    emit actualiserAffichage();
+}
+
+void Persistance::ajouterProgramme(QString saisieAtome, QString saisieProgramme) {
+    if (saisieAtome != NULL && saisieProgramme != NULL) {
+        if (saisieAtome == '0' || saisieAtome == '1' || saisieAtome == '2' || saisieAtome == '3' ||
+            saisieAtome == '4' ||
+            saisieAtome == '5' || saisieAtome == '6' || saisieAtome == '7' || saisieAtome == '8' ||
+            saisieAtome == '9' ||
+            saisieAtome == '+' || saisieAtome == '-' || saisieAtome == '*' || saisieAtome == '/' ||
+            saisieAtome == "CLEAR" || saisieAtome == "EVAL") {
+            throw ComputerException("Cet atome ne peut pas être réutilisé.");
+        } else {
+            if (!saisieProgramme.contains('[') || !saisieProgramme.contains(']'))
+                throw ComputerException("Ajouter les [ ] au programme");
+            if (mapVariable.contains(saisieAtome))
+                throw ComputerException("Ce nom est déjà lié à une variable");
+            mapProgramme.insert(saisieAtome, saisieProgramme);
+            emit actualiserAffichage();
+            return;
+        }
+    } else {
+        throw ComputerException("Remplissez tous les champs avant de valider.");
+    }
+}
+
+void Persistance::supprimerProgramme(QString atome) {
+    mapProgramme.remove(atome);
+    emit actualiserAffichage();
+}
---
+++ code/Persistance.h
@@ -0,0 +1,50 @@
+//
+// Created by Ismail Kadiri on 29/12/2020.
+//
+
+#ifndef PROJET_LO21_PERSISTANCE_H
+#define PROJET_LO21_PERSISTANCE_H
+
+#ifndef PERSISTENCE_H
+#define PERSISTENCE_H
+
+#include <QMap>
+#include <QWidget>
+
+class Persistance : public QObject {
+Q_OBJECT
+    static Persistance instance;
+
+    Persistance() = default;
+
+    QMap<QString, QString> mapVariable;
+    QMap<QString, QString> mapProgramme;
+public:
+
+    void ajouterVariable(QString atome, QString variable);
+
+    void supprimerVariable(QString atome);
+
+    QMap<QString, QString> obtenirMapVariable() { return mapVariable; };
+
+    unsigned int obtenirTailleMapVariable() { return mapVariable.size(); };
+
+    void ajouterProgramme(QString saisieAtome, QString saisieProgramme);
+
+    void supprimerProgramme(QString atome);
+
+    QMap<QString, QString> obtenirMapProgramme() { return mapProgramme; };
+
+    unsigned int obtenirTailleMapProgramme() { return mapProgramme.size(); };
+
+    static Persistance &obtenirPersistance();
+
+signals:
+
+    void actualiserAffichage();
+};
+
+#endif // PERSISTENCE_H
+
+
+#endif //PROJET_LO21_PERSISTANCE_H
